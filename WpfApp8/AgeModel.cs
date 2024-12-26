6d3f457b0262dd2680540c3add4afee4ad4baf85
Starinin Andrey <starinin-andrey@ya.ru>
2024-12-26 21:35:25 +0300

    Initial commit

---
+++ WpfApp8/AgeModel.cs
@@ -0,0 +1,56 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace WpfApp8;
+
+public sealed class AgeModel : IDataErrorInfo, INotifyPropertyChanged
+{
+    private string _error = string.Empty;
+    public string Error
+    {
+        get => _error;
+        private set => SetField(ref _error, value);
+    }
+    public int? Age { get; set; }
+    
+    public bool HasError => !string.IsNullOrEmpty(Error);
+
+    public string this[string columnName]
+    {
+        get
+        {
+            Error = string.Empty;
+            switch (columnName)
+            {
+                case "Age":
+                    Error = Age switch
+                    {
+                        null => "Ошибка ввода возраста",
+                        < 0 => "Возраст не может быть отрицательным!",
+                        _ => Error
+                    };
+
+                    break;
+                default:
+                    break;
+            }
+
+            return Error;
+        }
+    }
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    private void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+
+    private bool SetField<T>(ref T field, T value, [CallerMemberName] string? propertyName = null)
+    {
+        if (EqualityComparer<T>.Default.Equals(field, value)) return false;
+        field = value;
+        OnPropertyChanged(propertyName);
+        return true;
+    }
+}
---
+++ WpfApp8/MainWindowViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace WpfApp8;
+
+public class MainWindowViewModel : ViewModelBase
+{
+    private AgeModel _ageModel;
+
+    public AgeModel AgeModel
+    {
+        get => _ageModel;
+        set => SetField(ref _ageModel, value);
+    }
+
+    public ICommand CommandSave { get; }
+
+    public MainWindowViewModel()
+    {
+        _ageModel = new AgeModel();
+        
+        CommandSave = new RelayCommand(
+            _ =>
+        {
+            MessageBox.Show($"{AgeModel.Age} years old");
+        },
+        _ => !AgeModel.HasError);
+    }
+}
